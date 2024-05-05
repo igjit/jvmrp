ea60d4645818fa9ed3766a5f804b27d11422c018
igjit <igjit1@gmail.com>
2024-05-05 23:52:03 +0900

    Add test

---
+++ tests/test_reader.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import pytest
+
+from jvmrp.reader import read_class
+
+
+def test_read_class():
+    class_file = Path(__file__).parent / "data" / "Hello.class"
+
+    with open(class_file, "rb") as f:
+        cls = read_class(f)
+
+    assert cls["magic"] == b"\xca\xfe\xba\xbe"
