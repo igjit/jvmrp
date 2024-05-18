7f608448aacc32784d0a22ef7af4c1071e5057ee
igjit <igjit1@gmail.com>
2024-05-19 00:29:01 +0900

    Refactor: extract to method

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -6,12 +6,15 @@ from jvmrp.reader import read_class
 from jvmrp.vm import VM
 
 
-def test_execute_hello(capsys):
-    class_file = Path(__file__).parent / "data" / "Hello.class"
+def execute_class_file(class_file):
     java_class = read_class(class_file)
-
     vm = VM()
     vm.execute(java_class)
+
+
+def test_execute_hello(capsys):
+    class_file = Path(__file__).parent / "data" / "Hello.class"
+    execute_class_file(class_file)
 
     captured = capsys.readouterr()
     assert captured.out == "Hello, world.\n"
